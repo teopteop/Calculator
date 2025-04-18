179eb4bc9f80474479a9537cab77f511fbd2afe7
teopteop <teopteop0718@gmail.com>
2025-04-18 13:21:21 +0900

    도전과제 시도중

---
+++ src/lv3/ArithmeticCalculator.java
@@ -0,0 +1,13 @@
+package lv3;
+
+import static lv3.OperatorType.*;
+
+public class ArithmeticCalculator {
+    public static void main(String[] args) {
+
+        Object obj = DIVINED;
+        System.out.println(obj);
+
+        //
+    }
+}
---
+++ src/lv3/OperatorType.java
@@ -0,0 +1,5 @@
+package lv3;
+
+public enum OperatorType {
+    PLUS, MINUS, TIMES, DIVINED
+}
